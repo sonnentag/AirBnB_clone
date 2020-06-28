93c740657b2316d8a4c619f962a4339af41d043e
Tony Sims <zocle@zocle.net>
2020-06-28 13:40:39 -0500

    working on key __class__

---
+++ models/base_model.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""base module"""
+import json
+import uuid
+from datetime import datetime
+
+
+class BaseModel():
+    """Base class"""
+
+    def __class__(self):
+        pass
+
+    def __init__(self):
+        """init"""
+
+        self.id = str(uuid.uuid4())
+        self.created_at = str(datetime.now())
+        self.updated_at = str(datetime.now())
+
+    def __str__(self):
+        """return object as printable string"""
+
+        return ("[{}] ({}) {}".format(type(self).__name__,self.id,self.__dict__))
+
+    def save(self):
+        """save"""
+
+        self.updated_at = str(datetime.now())
+
+    def to_dict(self):
+        """return all k/v pairs as dict
+            UNFINISHED
+        return self.__dict__
+        """
+        return dict(
+            (key, value)
+            for (key, value) in self.__dict__.items()
+            )
