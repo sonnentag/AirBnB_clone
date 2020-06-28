0f5af25f8934052d48fa7fc2abef4193d58b8aba
Tony Sims <zocle@zocle.net>
2020-06-28 17:03:44 -0500

    almost 3

--- models/base_model.py
+++ models/base_model.py
@@ -15,8 +15,8 @@ class BaseModel():
         """init"""
 
         self.id = str(uuid.uuid4())
-        self.created_at = str(datetime.now())
-        self.updated_at = str(datetime.now())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def __str__(self):
         """return object as printable string"""
@@ -26,13 +26,13 @@ class BaseModel():
     def save(self):
         """save"""
 
-        self.updated_at = str(datetime.now())
+        self.updated_at = datetime.now()
 
     def to_dict(self):
         """return all k/v pairs as dict
         """
         self.__class__ = str(type(self).__name__)
         return dict(
-            (key, value)
+            (key, value) if type(value) in ['int','str'] else (key, str(value))
             for (key, value) in self.__dict__.items()
             )
