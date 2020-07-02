02cdb54646b7b08e237982e58368614798f335fa
Tony Sims <zocle@zocle.net>
2020-07-02 01:01:24 -0500

    (re)fixed date time for 4

--- models/base_model.py
+++ models/base_model.py
@@ -16,16 +16,17 @@ class BaseModel():
 
         from models import storage
         self.id = str(uuid.uuid4())
-        datevar = datetime.now
-        self.created_at = self.updated_at = datetime.now().isoformat()
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
         if kwargs:
             for k in kwargs:
-                if k not in ['__class__']:
-                    if k in ['created_at', 'update_at']:
-                        datevar = datetime.fromisoformat(kwargs[k])
+                if k != '__class__':
+                    if k in ['created_at', 'updated_at']:
+                        print(kwargs[k])
+                        var = datetime.fromisoformat(kwargs[k])
                     else:
-                        datevar = kwargs[k]
-                    setattr(self, k, datevar)
+                        var = kwargs[k]
+                    setattr(self, k, var)
         else:
             storage.new(self)
 
@@ -39,7 +40,7 @@ class BaseModel():
         """save"""
 
         from models import storage
-        self.updated_at = datetime.now().isoformat()
+        self.updated_at = datetime.now()
         storage.save()
 
     def to_dict(self):
@@ -47,6 +48,7 @@ class BaseModel():
         """
         self.__class__ = str(type(self).__name__)
         return dict(
-            (key, value) if type(value) in [int, str] else (key, str(value))
+            (key, value) if type(value) in [int, str]
+            else (key, datetime.isoformat(value))
             for (key, value) in self.__dict__.items()
             )
